f5232f5382add5b5a9b9e0aeded48261beeb3ae5
damian.fortunesky <damianfortunesky@gmail.com>
2023-06-21 21:13:51 -0300

    Se agrega estructura del proyecto

--- TpFinal.py
+++ TpFinal.py
@@ -1,2 +1,43 @@
 test = hola
 
+from modulos.menu_funciones import mostrar_menu, opcion1, opcion2, opcion3, opcion4, mostrar_mensaje_error, mensaje_dato_no_encontrado
+
+estado = True
+estado_menu = True
+
+while estado:
+
+    if estado_menu:
+        mostrar_menu()
+        estado_menu = False
+    
+    try:
+        opcion = int(input("Elige el numero de opción deseada: "))
+        estado = False
+    except ValueError:
+           mostrar_mensaje_error()
+    else:
+        opciones = {
+            1: opcion1,
+            2: opcion2,
+            3: opcion3,
+            4: opcion4
+        }
+
+        if opcion in opciones:
+            opciones[opcion]()
+            if opcion == 4:
+                estado = False      # Salir del bucle si la opción es 4
+                estado_menu = True
+            else:
+                estado = True
+                estado_menu = True
+        else:
+            mensaje_dato_no_encontrado()
+            estado = True
+            estado_menu = True
+
+
+        
+
+# correr app -> python TpFinal.py
---
+++ modulos/menu_funciones.py
@@ -0,0 +1,56 @@
+def mostrar_menu():
+    print("                           ")
+    print("Inicio del programa        ")
+    print("╔═════════════════════════╗")
+    print("║      Bienvenido a       ║")
+    print("║    Biblioteca IFTS24    ║")
+    print("╠═════════════════════════╣")
+    print("║ 1. Opción 1             ║")
+    print("║ 2. Opción 2             ║")
+    print("║ 3. Opción 3             ║")
+    print("║ 4. Salir                ║")
+    print("╚═════════════════════════╝")
+    print("                           ")
+
+def opcion1():
+    print("                            ")
+    print("Has seleccionado la opción 1")
+    print("Fin del programa            ")
+    print("                            ")
+
+def opcion2():
+    print("                            ")
+    print("Has seleccionado la opción 2")
+    print("Fin del programa            ")
+    print("                            ")
+
+def opcion3():
+    print("                            ")
+    print("Has seleccionado la opción 3")
+    print("Fin del programa            ")
+    print("                            ")
+
+def opcion4():
+    print("                       ")
+    print("Hasta luego!           ")
+    print("Fin del programa       ")
+    print("                       ")
+
+# Función para mostrar un mensaje de error
+def mostrar_mensaje_error():
+    print("                                        ")
+    print("╔══════════════════════════════════════╗")
+    print("║      Error al ingresar dato !        ║")
+    print("╠══════════════════════════════════════╣")
+    print("║ Debe ingresar un Nº de opción válido ║")
+    print("╚══════════════════════════════════════╝")
+    print("                                        ")
+
+def mensaje_dato_no_encontrado():
+    print("                                        ")
+    print("╔══════════════════════════════════════╗")
+    print("║         Error de busqueda !          ║")
+    print("╠══════════════════════════════════════╣")
+    print("║         Esa opcion no existe         ║")
+    print("╚══════════════════════════════════════╝")
+    print("                                        ")
